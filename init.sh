5a36e7d8a3ebb380665027c4df18b4fdd5a7ad3e
Jacek Tomaszewski <kontakt@jtomaszewski.eu>
2013-10-31 22:55:59 +0100

    fix

--- init.sh
+++ init.sh
@@ -1,31 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
 # sudo apt-get install -y git-core
 
-cd $HOME
-git clone https://github.com/jtomaszewski/.dotfiles
-
-cd .dotfiles
-./update.sh
-
-read -p "Run install.sh (need sudo rights)? [yn]" answer
-if [[ $answer = y ]] ; then
+read -p "run install.sh? (needed sudo rights)" answer
+if [[ "$answer" = "y" ]] ; then
   sudo ./install.sh
 fi
 
 read -p "default shell: fish/zsh? [yn]" answer
-if [[ $answer = 'zsh' ]] ; then
-  if [[ which zsh ]]; then
+if [ "$answer" = "zsh" ]; then
+  if [ -n `which zsh` ]; then
     chsh -s `which zsh`
   else
     echo "no zsh isntalled!"
   fi
-else
+elif [ "$answer" = "fish" ]; then
   echo "NOTE: you need to install fish2 manually (see their website)"
-  if [[ which fish ]]; then
-    echo "no fish installed!"
-  else
+  if [ -n `which fish` ]; then
     chsh -s /usr/bin/fish
+  else
+    echo "no fish installed!"
   fi
 fi
 
