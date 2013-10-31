9972aae51468ec3e8ffe9ed2a153efe41f3346e4
Jacek Tomaszewski <kontakt@jtomaszewski.eu>
2013-10-31 20:53:05 +0100

    fix

--- init.sh
+++ init.sh
@@ -10,7 +10,11 @@ git clone https://github.com/jtomaszewski/.dotfiles
 cd .dotfiles/
 git submodule update --init --recursive
 
-sudo ./install.sh
+read -p "Run install.sh script?" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  sudo ./install.sh
+fi
 
 chsh -s /usr/bin/fish
 ./create_symlinks.sh
