0217e4cfb3b8187fef6558a0505f81372f86cf39
Jacek Tomaszewski <kontakt@jtomaszewski.eu>
2013-10-31 21:10:19 +0100

    fix

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -11,7 +11,7 @@ files="gitconfig gitignore_global config/fish pryrc rspec bashrc bash_aliases oh
 echo "Existing dotfiles will be moved to $backup_dir."
 mkdir -p $backup_dir
 
-mkdir -p $backup_dir/old/config
+mkdir -p $backup_dir/config
 mkdir -p $HOME/.config
 
 for file in $files; do
