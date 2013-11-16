c63a0d252ec1b0841df3e3c9685a46cbd470218f
Jacek Tomaszewski <kontakt@jtomaszewski.eu>
2013-11-16 17:25:54 +0100

    add bash_it

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -5,7 +5,7 @@ dir=$HOME/.dotfiles
 backup_dir=$dir/old
 
 # list of files/folders to symlink in homedir
-files="dir_colors bin gitconfig gitignore_global config/fish pryrc rspec bashrc bash_aliases oh-my-fish oh-my-zsh zshrc profile bash_profile tmux.conf"
+files="bash_it dir_colors bin gitconfig gitignore_global config/fish pryrc rspec bashrc bash_aliases oh-my-fish oh-my-zsh zshrc profile bash_profile tmux.conf"
 ##########
 
 echo "Existing dotfiles will be moved to $backup_dir."
