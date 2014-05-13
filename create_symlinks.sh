49ba79e1ceb89b320f801a4e1bdde9e984337b33
Jacek Tomaszewski <jacek@jtom.me>
2014-05-13 14:21:27 +0200

    Fix .config/fish

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -13,26 +13,51 @@ BACKUP_DIR="$DOTFILES_REPOSITORY_DIR/old/`timestamp`"
 echo "Existing dotfiles will be moved to $BACKUP_DIR."
 mkdir -p $BACKUP_DIR
 
-for file in $DOTFILES_DIR/.* $DOTFILES_DIR/*; do
-  filename=`basename $file`
-  source="$file"
-  target="$HOME/$filename"
-  backup="$BACKUP_DIR/$filename"
-  
+create_symlink() {
+  source=$1
+  target=$2
+  backup=$3
+  filename=`basename $source`
+
   echo "Creating symlink to \"$filename\" in home directory."
 
-  # Remove if it's a symlink
+  # # Remove if it's a symlink
   if [[ -h $target ]]; then
     echo "Existing symlink to \"$filename\" has been removed."
     rm $target
   fi
 
-  # Move, if it exists
+  # # Move, if it exists
   if [[ -e $target ]]; then
-    echo "Existing \"$filename\" has been moved to \"$BACKUP_DIR\"."
-    rm -rf $backup
+    echo "Existing \"$filename\" has been moved to \"$backup\"."
     mv $target $backup
   fi
 
   ln -s $source $target 
+}
+
+for file in $DOTFILES_DIR/.* $DOTFILES_DIR/*; do
+  filename=`basename $file`
+
+  # Skip .config directory
+  if [ "$filename" = "*" ] || [ "$filename" = "." ] || [ "$filename" = ".." ] || [ "$filename" = ".config" ]; then
+    continue
+  fi
+
+  create_symlink $file "$HOME/$filename" "$BACKUP_DIR/$filename"
+done
+
+# # We'll handle .config files recursively
+for subdirectory in .config; do
+  for file in $DOTFILES_DIR/$subdirectory/.* $DOTFILES_DIR/$subdirectory/*; do
+    filename=`basename $file`
+
+    # Skip .config directory
+    if [ "$filename" = "*" ] || [ "$filename" = "." ] || [ "$filename" = ".." ]; then
+      continue
+    fi
+
+    mkdir -p "$BACKUP_DIR/$subdirectory"
+    create_symlink $file "$HOME/$subdirectory/$filename" "$BACKUP_DIR/$subdirectory/$filename"
+  done
 done
